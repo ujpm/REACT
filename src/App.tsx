9ab913cdc9214126dde63182bca5f55c50f5ba21
ujpm <uwizeyimanajp2@gmail.com>
2025-02-28 10:19:05 +0200

    －O－ finally deployind demo to cloudflare==succesfully

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import { ThemeProvider, CssBaseline } from '@mui/material';
-import { Provider } from 'react-redux';
-import { store } from './store';
 import theme from './theme';
 import Layout from './components/layout/Layout';
 import LandingPage from './pages/Landing/LandingPage';
@@ -10,6 +8,7 @@ import Dashboard from './pages/Dashboard/Dashboard';
 import Login from './pages/Auth/Login';
 import Register from './pages/Auth/Register';
 import ReportIssue from './pages/ReportIssue/ReportIssue';
+import { store } from './store';
 
 // Protected Route component
 const ProtectedRoute: React.FC<{ children: React.ReactNode }> = ({ children }) => {
@@ -20,42 +19,35 @@ const ProtectedRoute: React.FC<{ children: React.ReactNode }> = ({ children }) =
 // App component
 const App: React.FC = () => {
   return (
-    <Provider store={store}>
-      <Router>
-        <ThemeProvider theme={theme}>
-          <CssBaseline />
-          <Layout>
-            <Routes>
-              {/* Public routes */}
-              <Route path="/" element={<LandingPage />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/register" element={<Register />} />
-
-              {/* Protected routes */}
-              <Route
-                path="/dashboard"
-                element={
-                  <ProtectedRoute>
-                    <Dashboard />
-                  </ProtectedRoute>
-                }
-              />
-              <Route
-                path="/report-issue"
-                element={
-                  <ProtectedRoute>
-                    <ReportIssue />
-                  </ProtectedRoute>
-                }
-              />
-
-              {/* Catch all route */}
-              <Route path="*" element={<Navigate to="/" replace />} />
-            </Routes>
-          </Layout>
-        </ThemeProvider>
-      </Router>
-    </Provider>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Layout>
+        <Routes>
+          {/* Public routes */}
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          
+          {/* Protected routes */}
+          <Route
+            path="/dashboard"
+            element={
+              <ProtectedRoute>
+                <Dashboard />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/report-issue"
+            element={
+              <ProtectedRoute>
+                <ReportIssue />
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </Layout>
+    </ThemeProvider>
   );
 };
 
